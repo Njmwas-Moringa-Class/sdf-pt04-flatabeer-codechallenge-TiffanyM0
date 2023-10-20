ed5e97fc527ae809a5c69741fb9f835dced8ad1f
TIFFANY MAINA <tiffmaina@gmail.com>
2023-10-20 23:33:48 +0300

    First Deliverable

--- src/index.js
+++ src/index.js
@@ -1,33 +1,86 @@
 // Code here
 
-// fetch request for info from bd, record input value as ul
+fetch("http://localhost:3000/beers/1")
+  .then((response) => response.json())
+  .then((data) => {
+    let beer1 = document.querySelector("#beer-name");
+    beer1.innerText = `${data.name}`;
 
-// const BASE_URL = "http://localhost:3000"
-// console.log('here we are now');
+    let beer1Img = document.querySelector("img#beer-image");
+    console.log(beer1Img);
+    beer1Img.setAttribute("alt", `${data.name}`);
+
+    let beer1ImgSrc = document.querySelector("img#beer-image");
+    console.log(beer1ImgSrc);
+    beer1ImgSrc.setAttribute("src",`${data.image_url}`);
+
+    let beer1Desc = document.querySelector("#beer-description");
+    beer1Desc.innerText = `${data.description}`;
+    console.log(beer1Desc);
+
+    let beerList = document.querySelector("#beer-list");
+
+  })
+;
+
+
+// ====== a list of beer names in the nav bar at the left =======//
 
 async function getBeers() {
   const beersResponse = await fetch("http://localhost:3000/beers");
   const beers = await beersResponse.json();
-  console.log(beersResponse);
-  console.log(beers);
-
+  let beerList = document.querySelector("#beer-list");
+  beerList.appendChild("li").innerText(`${beers.name}`)
+  return beers;
 }
 
-function beerListInNav(beer) {
-  const insertNames = document.createElement("li");
-  console.log(insertNames);
-  insertNames.innerText = beer.name;
-  return insertNames;
+// fetch request for info from bd, record input value as ul
+// const BASE_URL = "http://localhost:3000"
+// console.log('here we are now');
+
+function updateReview(){
+  const newReview =  document.getElementById("review-form");
+  newReview.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const newReviewBeer = document.getElementById("review"); 
+    console.log(newReviewBeer);
+  })
 }
+
+// function beerListInNav(beer) {
+//   const beerList = document.querySelector("#beer-list");
+//   beerList.appendChild("li");
+//   return `<li>${beer.name}</li>`;
+// }
+// document.addEventListener("DOMContentLoaded", () => {
+//   getBeers();
+//   beerListInNav();
+// });
+
 // /*
 //     to run the code after DOM has loaded, call functions in the below callbackfn
 // */
-document.addEventListener("DOMContentLoaded", () => {
-  const headingList = document.getElementById("beer-list");
-  getBeers()
-});
 
+// document.addEventListener("DOMContentLoaded", () => {
+//   const headingList = document.getElementById("beer-list");
+//   getBeers().then((beers) => {
+//     console.log(beers);
+//     let div1 = document.createElement("div");
+//     div1.className = "beer1";
+//     div1.innerHTML = `
+//         <h2>${data.name}</h2>
+//         <img src= "${data.image_url}"
+//         <p>${data.description}</p>
+//     `;
+//     document.querySelector("#beer-list").appendChild(div1);
+//   });
+// });
 
+// let beerImage = document.querySelector("div.beer-details img.src");
+// let beerDescription = document.querySelector("#beer-description");
+// beerName.innerText = beers.name;
+// beerImage.innerText = beers.image_url;
+// beerDescription.innerText = beers.description;
 
 // async function getBeers() {
 //   return fetch("http://localhost:3000/beers").then(() => {
@@ -45,36 +98,4 @@ document.addEventListener("DOMContentLoaded", () => {
 //     const input = document.querySelector("#review");
 // })
 
-fetch("http://localhost:3000/beers")
-.then((Response) => Response.json())
-.then (data => {
-    let div1 = document.createElement("div");
-    div1.className = "beer1"
-    div1.innerHTML = `
-        <h2>${data.name}</h2>
-        <img src= "${data.image_url}"
-        <p>${data.description}</p>
-    `
-    document.querySelector("#beer-list").appendChild(div1);
-    
-    // let beerImage = document.querySelector("div.beer-details img.src");
-    // let beerDescription = document.querySelector("#beer-description");
-    // beerName.innerText = beers.name;
-    // beerImage.innerText = beers.image_url;
-    // beerDescription.innerText = beers.description;
-})
 // console.log(fetch())
-
-// fetch("http://localhost:3000/beers/1")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     let div1 = document.querySelector("#beer-details");
-//     div1.innerHTML = "";
-//     div1.innerHTML = `
-//     <h2 id="beer-name">${data.name}</h2>
-//     <img
-//           id="beer-image"
-//           alt="${data.name}"
-//           src="${data.image_url}"
-//     />`;
-//   });
